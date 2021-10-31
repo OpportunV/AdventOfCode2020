0eeb655ac9cf5aa4718d47ef46fd631aea463317
OpportunV <RsTGear@gmail.com>
2021-10-31 21:55:58 +0300

    day 5

--- AdventOfCode2020/Days/Day5.cs
+++ AdventOfCode2020/Days/Day5.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using AdventOfCode2020.Helpers;
 
 namespace AdventOfCode2020.Days
@@ -20,7 +23,16 @@ namespace AdventOfCode2020.Days
                 return -1;
             }
 
-            return -1;
+            var max = 0;
+            
+            foreach (var line in lines)
+            {
+                var curId = GetSeatId(line);
+            
+                max = Math.Max(max, curId);
+            }
+
+            return max;
         }
         
         public static object Part2()
@@ -34,8 +46,53 @@ namespace AdventOfCode2020.Days
             {
                 return -1;
             }
+
+            var seatIds = new HashSet<int>(lines.Select(GetSeatId));
+
+            for (int i = 1; i < 127 * 7; i++)
+            {
+                if (!seatIds.Contains(i) && seatIds.Contains(i + 1) && seatIds.Contains(i - 1))
+                {
+                    return i;
+                }
+            }
+            
             
             return -1;
+        }
+        
+        private static int GetSeatId(string line)
+        {
+            var curMax = 127;
+            var curMin = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (line[i] == 'F')
+                {
+                    curMax -= (curMax - curMin + 1) / 2;
+                }
+                else
+                {
+                    curMin += (curMax - curMin + 1) / 2;
+                }
+            }
+            
+            var curRow = curMin;
+            curMax = 7;
+            curMin = 0;
+            for (int i = 7; i < 10; i++)
+            {
+                if (line[i] == 'L')
+                {
+                    curMax -= (curMax - curMin + 1) / 2;
+                }
+                else
+                {
+                    curMin += (curMax - curMin + 1) / 2;
+                }
+            }
+            
+            return curMin + curRow * 8;
         }
     }
 }
